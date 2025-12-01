34b5a26b8ae088303a555aefe1c5ca2f29a5a8fe
Patrick Gutorski <patrick@ebaf7cdf-d0c6-402f-ab5a-6564b2e2ae3b.fritz.box>
2025-12-01 16:39:02 +0100

    Initial commit

---
+++ src/main/java/com/example/javaspringbackend/JavaspringbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaspringbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaspringbackend/JavaspringbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
